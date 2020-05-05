c7cc6b3b9e24209dacfb0c9bc48cedcc6ccb3502
ch2isk4kos <ch2isk4kos@gmail.com>
2020-05-05 13:54:56 -0400

    built crud actions for courts

--- app/controllers/api/v1/courts_controller.rb
+++ app/controllers/api/v1/courts_controller.rb
@@ -1,2 +1,33 @@
 class Api::V1::CourtsController < ApplicationController
+
+    before_action :find_court, only: [:show, :update, :destroy]
+
+    def index
+        @courts = Court.all
+        render json: @courts, status: 200
+    end
+
+    def show
+        render json: @court, status: 200
+    end
+
+    def create
+        @court = Court.new(court_params)
+        if @court.save
+            render json: @court, status: 200
+        else
+            render json: {error: "Something went completely wrong"}
+        end
+    end
+
+    private
+
+    def find_court
+        @court = Court.find_by(params[:id])
+    end
+
+    def court_params
+        params.require(:court).permit(:name, :address, :img_url, :type)
+    end
+
 end
